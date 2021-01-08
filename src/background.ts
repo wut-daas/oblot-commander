a6f37bc2133b61b9e8e7129ba95f222018fb8f09
Marek Łukasiewicz <marek@lukasiewicz.tech>
2021-01-08 09:31:29 +0100

    Add serialport

--- src/background.ts
+++ src/background.ts
@@ -5,6 +5,9 @@ import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
+// SerialPort fix according to https://mechawrench.com/posts/electron-vue-serialport-implementation/
+app.allowRendererProcessReuse = false;
+
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
