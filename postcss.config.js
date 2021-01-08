e03bd7403e516eeae32b7bb817ed2f6f7903ee3e
Marek Łukasiewicz <marek@lukasiewicz.tech>
2021-01-08 09:16:51 +0100

    Add purgecss vue-cli plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    require("tailwindcss"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./src/**/*.vue", "./public/index.html"],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
 };
